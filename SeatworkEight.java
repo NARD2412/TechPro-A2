05bf9690d50b97832452483a1fc92e9124308fa9
NARD2412 <glennard123ps@gmail.com>
2025-09-08 09:05:06 +0000

    SEATWORK 8

--- SeatworkEight.java
+++ SeatworkEight.java
@@ -1,18 +1,18 @@
-public class SeatworkSeven {
+public class SeatworkEight {
             public static void main(String args[]) {
-                    char grade = 'C';
+                    int grade = 3;
                             switch (grade) {
-                                        case 'A':
+                                        case 1:
                                                         System.out.println("Excellent!");
                                                                         break;
-                                                                                    case 'B':
-                                                                                                case 'C':
+                                                                                    case 2:
+                                                                                                case 3:
                                                                                                                 System.out.println("Well done");
                                                                                                                                 break;
-                                                                                                                                            case 'D':
+                                                                                                                                            case 4:
                                                                                                                                                             System.out.println("You passed");
                                                                                                                                                                             break;
-                                                                                                                                                                                        case 'F':
+                                                                                                                                                                                        case 5:
                                                                                                                                                                                                         System.out.println("Better try again");
                                                                                                                                                                                                                         break;
                                                                                                                                                                                                                                     default:
@@ -21,3 +21,4 @@ public class SeatworkSeven {
                                                                                                                                                                                                                                                                     System.out.println("Your grade is " + grade);
                                                                                                                                                                                                                                                                         }
                                                                                                                                                                                                                                                                         }
+                                                                                                                                                                                                                                                                        
